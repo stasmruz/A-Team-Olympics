9bbe4ba53a42b7fad656c77024010b0deb6a689d
sak0213 <steven.kitay0123@gmail.com>
2021-10-02 00:17:42 -0400

    flask attempt

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, json, jsonify
+from flask.wrappers import Response
+import pymongo
+
+app = Flask(__name__)
+
+# setup mongo connection
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+# connect to mongo db and collection
+db = client.Olympic_data
+tester = db.Barchart
+
+@app.route("/api/test", methods = ["GET"])
+def api_output():
+    try:
+        return f'{tester.find({})}'
+    except Exception as ex:
+        print(ex)
+        return Response(
+            response= json.dumps({"message":"Messed up"}),
+            status = 500
+        )
+
+
+@app.route("/")
+def welcome():
+    return (
+        f"Index<br/>"
+        f"Available Routes:<br/>"
+        f"/api/test<br/>"
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
