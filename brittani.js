4241ff3de837283757568ba52a67ad5f916f3719
brittanijoi94 <brittanijoi@yahoo.com>
2021-10-04 21:17:34 -0400

    test

--- brittani.js
+++ brittani.js
@@ -1,5 +1,5 @@
 console.log("searchResults: ", searchResults);
-default_filtered=searchResults.filter(result=>(result.Sport==="Athletics"));
+default_filtered=searchResults.filter(result=>(result.Sport==="Gymnastics"));
 console.log(Object.entries(searchResults));
 const labels = default_filtered.map(result=>result.Year);
 
@@ -11,7 +11,7 @@ const data = {
           if (result.Sex==="M"){
               return result.Wins;
           } else {
-              return null
+              return 0
           }
       }),
       fill: false,
@@ -24,7 +24,7 @@ const data = {
             if (result.Sex==="F"){
                 return result.Wins;
             } else {
-                return null
+                return 0
             }
         }),
         fill: false,
@@ -42,23 +42,53 @@ const config = {
 let ctx=document.getElementById("plot").getContext("2d");
 let chart=new Chart(ctx,config)
 
-// chart.destroy()
+d3.selectAll("#selDataset").on("change", updatePlotly);
 
+// This function is called when a dropdown menu item is selected
+function updatePlotly() {
+  // Use D3 to select the dropdown menu
+  var dropdownMenu = d3.select("#selDataset");
+  // Assign the value of the dropdown menu option to a variable
+  var dataset = dropdownMenu.property("value");
+  let default_filtered=searchResults.filter(result=>(result.Sport===`${dataset}`));
+}
 
+// function updateChart(){
+//     chart.destroy()
 // console.log("searchResults: ", searchResults);
-// default_filtered=searchResults.filter(result=>(result.Sport==="Gymnastics"&&result.Sex==="M"));
+// default_filtered=searchResults.filter(result=>(result.Sport==="Aeronautics"));
 // console.log(Object.entries(searchResults));
 // const labels = default_filtered.map(result=>result.Year);
 
 // const data = {
 //     labels: labels,
 //     datasets: [{
-//       label: 'Gender Over Time',
-//       data: default_filtered.map(result=>result.Wins),
+//       label: 'Male',
+//       data: default_filtered.map(result=>{
+//           if (result.Sex==="M"){
+//               return result.Wins;
+//           } else {
+//               return 0
+//           }
+//       }),
 //       fill: false,
 //       borderColor: 'rgb(75, 192, 192)',
 //       tension: 0.1
-//     }]
+//     },
+//     {
+//         label: 'Female',
+//         data: default_filtered.map(result=>{
+//             if (result.Sex==="F"){
+//                 return result.Wins;
+//             } else {
+//                 return 0
+//             }
+//         }),
+//         fill: false,
+//         borderColor: 'rgb(75, 192, 192)',
+//         tension: 0.1
+//       }
+// ]
 //   };
 
 // const config = {
@@ -68,5 +98,7 @@ let chart=new Chart(ctx,config)
 
 // let ctx=document.getElementById("plot").getContext("2d");
 // let chart=new Chart(ctx,config)
+// 
 
-// // chart.destroy()
+
+
