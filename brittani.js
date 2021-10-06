c75bca3cdd3d043ef1443a83adec31be5cf46fb9
brittanijoi94 <brittanijoi@yahoo.com>
2021-10-05 20:20:45 -0400

    gender

--- brittani.js
+++ brittani.js
@@ -1,34 +1,61 @@
 console.log("searchResults: ", searchResults);
-default_filtered=searchResults.filter(result=>(result.Sport==="Gymnastics"));
+let selector = d3.select("#selDataset");
+let options = []
+default_filtered=searchResults.filter(result=>{
+    if (!options.includes(result.Sport)){
+        options.push(result.Sport)
+        selector
+        .append("option")
+        .text(result.Sport)
+        .property("value", result.Sport);
+    }
+    
+    return result.Sport==="Gymnastics"
+});
 console.log(Object.entries(searchResults));
-const labels = default_filtered.map(result=>result.Year);
+const years= default_filtered.map(result=>result.Year);
+const labels=[]
+years.forEach(year=> {
+    if (!labels.includes(year)){
+        labels.push(year)
+    }
+})
+console.log(labels)
+let male_data=[]
+let female_data=[]
+console.log(default_filtered)
+labels.forEach(year=>{
+    let year_filter=default_filtered.filter(data=>data.Year=== year)
+    let males=year_filter.filter(data=>data.Sex==="M")
+    let females=year_filter.filter(data=>data.Sex==="F")
+    if (males.length>0){
+        male_data.push(males[0].Wins)
+    } else {
+        male_data.push(null)
+    }
+    if (females.length>0){
+        female_data.push(females[0].Wins)
+    } else {
+        female_data.push(null)
+    }
+    
+})
+console.log(male_data, female_data)
 
 const data = {
     labels: labels,
     datasets: [{
       label: 'Male',
-      data: default_filtered.map(result=>{
-          if (result.Sex==="M"){
-              return result.Wins;
-          } else {
-              return 0
-          }
-      }),
+      data: male_data,
       fill: false,
-      borderColor: 'rgb(75, 192, 192)',
+      borderColor: 'rgb(52, 64, 235)',
       tension: 0.1
     },
     {
         label: 'Female',
-        data: default_filtered.map(result=>{
-            if (result.Sex==="F"){
-                return result.Wins;
-            } else {
-                return 0
-            }
-        }),
+        data: female_data,
         fill: false,
-        borderColor: 'rgb(75, 192, 192)',
+        borderColor: 'rgb(235, 52, 159)',
         tension: 0.1
       }
 ]
@@ -37,68 +64,80 @@ const data = {
 const config = {
     type: 'line',
     data: data,
+    options: {
+        spanGaps: true 
+    }
   };
 
 let ctx=document.getElementById("plot").getContext("2d");
 let chart=new Chart(ctx,config)
 
-d3.selectAll("#selDataset").on("change", updatePlotly);
 
-// This function is called when a dropdown menu item is selected
-function updatePlotly() {
-  // Use D3 to select the dropdown menu
-  var dropdownMenu = d3.select("#selDataset");
-  // Assign the value of the dropdown menu option to a variable
-  var dataset = dropdownMenu.property("value");
-  let default_filtered=searchResults.filter(result=>(result.Sport===`${dataset}`));
+
+function updateChart(selection){
+    console.log(chart, selection)
+    chart.destroy()
+
+default_filtered=searchResults.filter(result=>(result.Sport===selection));
+console.log(Object.entries(searchResults));
+const years= default_filtered.map(result=>result.Year);
+const labels=[]
+years.forEach(year=> {
+    if (!labels.includes(year)){
+        labels.push(year)
+    }
+})
+console.log(labels)
+let male_data=[]
+let female_data=[]
+console.log(default_filtered)
+labels.forEach(year=>{
+    let year_filter=default_filtered.filter(data=>data.Year=== year)
+    let males=year_filter.filter(data=>data.Sex==="M")
+    let females=year_filter.filter(data=>data.Sex==="F")
+    if (males.length>0){
+        male_data.push(males[0].Wins)
+    } else {
+        male_data.push(null)
+    }
+    if (females.length>0){
+        female_data.push(females[0].Wins)
+    } else {
+        female_data.push(null)
+    }
+    
+})
+const data = {
+    labels: labels,
+    datasets: [{
+      label: 'Male',
+      data: male_data,
+      fill: false,
+      borderColor: 'rgb(52, 64, 235)',
+      tension: 0.1
+    },
+    {
+        label: 'Female',
+        data: female_data,
+        fill: false,
+        borderColor: 'rgb(7235, 52, 159)',
+        tension: 0.1
+      }
+]
+  };
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {
+        spanGaps: true 
+    }
+  };
+
+let ctx=document.getElementById("plot").getContext("2d");
+chart=new Chart(ctx,config)
 }
 
-// function updateChart(){
-//     chart.destroy()
-// console.log("searchResults: ", searchResults);
-// default_filtered=searchResults.filter(result=>(result.Sport==="Aeronautics"));
-// console.log(Object.entries(searchResults));
-// const labels = default_filtered.map(result=>result.Year);
-
-// const data = {
-//     labels: labels,
-//     datasets: [{
-//       label: 'Male',
-//       data: default_filtered.map(result=>{
-//           if (result.Sex==="M"){
-//               return result.Wins;
-//           } else {
-//               return 0
-//           }
-//       }),
-//       fill: false,
-//       borderColor: 'rgb(75, 192, 192)',
-//       tension: 0.1
-//     },
-//     {
-//         label: 'Female',
-//         data: default_filtered.map(result=>{
-//             if (result.Sex==="F"){
-//                 return result.Wins;
-//             } else {
-//                 return 0
-//             }
-//         }),
-//         fill: false,
-//         borderColor: 'rgb(75, 192, 192)',
-//         tension: 0.1
-//       }
-// ]
-//   };
-
-// const config = {
-//     type: 'line',
-//     data: data,
-//   };
-
-// let ctx=document.getElementById("plot").getContext("2d");
-// let chart=new Chart(ctx,config)
-// 
 
 
-
+// // chart.destroy()
