578dbdba43685b3b3de373d53223f356846bc6e6
Ken Tseng <84150117+Ken52093@users.noreply.github.com>
2021-10-06 19:43:34 -0400

    Ken

---
+++ templates/Ken.js
@@ -0,0 +1,15 @@
+function openMenu(){
+    document.getElementById('sideMenu').style.marginLeft ='0';
+    document.getElementById('main').style.marginLeft ='250px';
+    document.getElementById('expandArrow').style.marginLeft='250px'
+    
+
+}
+
+function closeMenu(){
+    document.getElementById('sideMenu').style.marginLeft ='-250px';
+    document.getElementById('main').style.marginLeft ='0';
+    document.getElementById('expandArrow').style.marginLeft='0'
+   
+
+}
