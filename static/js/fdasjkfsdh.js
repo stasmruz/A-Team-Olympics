effffe50511eadd24383f7c9f5349b8311bf9552
sak0213 <steven.kitay0123@gmail.com>
2021-10-07 18:01:11 -0400

    fixed app

--- static/js/fdasjkfsdh.js
+++ static/js/fdasjkfsdh.js
@@ -3,27 +3,27 @@ function init(){
     const data = {
     labels: labels,
     datasets: [{
-        label: 'Wins by Country',
+        label: 'Country Medal Count',
         data: countryResults.map(row=>row.Wins),
         backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(255, 205, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(201, 203, 207, 0.2)'
-        ],
-        borderColor: [
-        'rgb(255, 99, 132)',
-        'rgb(255, 159, 64)',
-        'rgb(255, 205, 86)',
-        'rgb(75, 192, 192)',
-        'rgb(54, 162, 235)',
-        'rgb(153, 102, 255)',
-        'rgb(201, 203, 207)'
-        ],
-        borderWidth: 1
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)'
+            ],
+            borderColor: [
+            'rgb(77, 166, 255)',
+            'rgb(136, 77, 255)',
+            'rgb(255, 51, 255)',
+            'rgb(255, 255, 0)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+            ],
+            borderWidth: 1
     }]
     };
 
